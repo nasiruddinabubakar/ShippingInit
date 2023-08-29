7c43caeaf2f8cd5c4abb74e10a3b527a81be5b15
Nasiruddin Abubakar <nasiruddinabubakar@gmail.com>
2023-08-30 01:06:47 +0500

    SignUp Component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { SignUp, ToggleLoginSignup } from "./components/SignUp/SignUp";
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Main">
+        <header>
+          <nav></nav>
+        </header>
+      <SignUp/>
+       
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
