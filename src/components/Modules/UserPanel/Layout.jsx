af90c676fb5462806cfcf41c44546520535d12a2
Nasiruddin Abubakar <111733598+nasiruddinabubakar@users.noreply.github.com>
2024-03-26 18:09:26 +0500

    ravamped user Layout

--- src/components/Modules/UserPanel/Layout.jsx
+++ src/components/Modules/UserPanel/Layout.jsx
@@ -1,44 +1,43 @@
-import { Anchor, ArrowRightLeft, Check,  Ship, Circle, User, LogOut } from "lucide-react";
-import styles from "./Layout.module.css";
-import { Header } from "../../UI/Header";
-import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Spinner from "../../UI/Spinner";
-import React from "react";
-import Popup from "reactjs-popup";
-import { toast } from "react-toastify";
+import { ArrowRightLeft, Check, Ship, User, LogOut } from 'lucide-react';
+import styles from './Layout.module.css';
+import { Header } from '../../UI/Header';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Spinner from '../../UI/Spinner';
+import React from 'react';
+
+import { toast } from 'react-toastify';
+import { SingleOrder } from './SingleOrder';
 // import "reactjs-popup/dist/index.css";
 export const Layout = () => {
   const [orders, setOrders] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
-  const [selectedOrder, setSelectedOrder] = useState(null);
+  const [singleOrder, setSingleOrder] = useState(null);
+  const [transitOrDelivered, setTransitOrDelivered] = useState(false);
+ 
   const navigate = useNavigate();
-    function handleLogout(){
+  function handleLogout() {
+    localStorage.clear();
+    navigate('/');
+  }
 
-        localStorage.clear();
-        navigate("/");
-
+  const onClickList = (e) => {
+    e.preventDefault();
+    if (e.target.value === 1) {
+      setTransitOrDelivered(false);
+    } else {
+      setTransitOrDelivered(true);
     }
-
-  const handleListItemClick = (order) => {
-    setSelectedOrder(order);
-    setIsPopupOpen(true);
-  };
-
-  const handlePopupClose = () => {
-    setSelectedOrder(null);
-    setIsPopupOpen(false);
+    console.log(e.target.value);
   };
 
   useEffect(() => {
     async function getOrders() {
       try {
         setIsLoading(true);
-        const res = await fetch("http://127.0.0.1:5000/api/orders/history", {
+        const res = await fetch('http://127.0.0.1:5000/api/orders/history', {
           headers: {
-            authorization: `${localStorage.getItem("user")}`,
+            authorization: `${localStorage.getItem('user')}`,
           },
         });
         const response = await res.json();
@@ -53,68 +52,85 @@ export const Layout = () => {
     getOrders();
   }, []);
 
-  async function onclickdel(booking_id){
-    
-console.log("hello transit");
-    const res = await fetch(
-      `http://127.0.0.1:5000/api/orders/${booking_id}`,
-      { method:"DELETE",
-        headers: {
-          authorization: `${localStorage.getItem("user")}`,
-        },
-      }
-    );
-     await res.json();
+  async function onclickdel(booking_id) {
+    console.log('hello transit');
+    const res = await fetch(`http://127.0.0.1:5000/api/orders/${booking_id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `${localStorage.getItem('user')}`,
+      },
+    });
+    await res.json();
   }
- 
+
   return (
     <div className={`Main ${styles.main}`}>
       <Header />
-      <div className={styles.window} >
-       <div className={styles.navli}>
-        <>
-       <ul>
-          <li><ArrowRightLeft size={24} color="#ffb545"/>Ongoing Orders </li>
-          <li> <Check strokeWidth={2.9} color="#00c46a"/>FullFilled Orders </li>
-          <li><User color="WHITE"/>Account Info</li>
-        </ul>
-        
-         <div style={{display:"flex",marginLeft:"0rem"}}className={styles.logout}>
-          <button onClick={handleLogout}>LogOut<LogOut /> </button>
-          </div>
+      <div className={styles.window}>
+        <div className={styles.navli}>
+          <>
+            <ul>
+              <li onClick={(e) => onClickList(e)} value={1}>
+                <ArrowRightLeft size={24} color="#ffb545" />
+                Ongoing Orders{' '}
+              </li>
+              <li onClick={(e) => onClickList(e)} value={2}>
+                {' '}
+                <Check strokeWidth={2.9} color="#00c46a" />
+                FullFilled Orders{' '}
+              </li>
+              <li>
+                <User color="WHITE" />
+                Account Info
+              </li>
+            </ul>
+
+            <div
+              style={{ display: 'flex', marginLeft: '0rem' }}
+              className={styles.logout}
+            >
+              <button onClick={handleLogout}>
+                LogOut
+                <LogOut />{' '}
+              </button>
+            </div>
           </>
-        
-        
-        
-       </div>
-       <div className={styles.orderWindow}>
-        <ul className={styles.orders}>
-        {isLoading ? (
-                <Spinner />
-              ) : (
-                orders.map((item) => {
-                  return (
-                    <li onClick={() => handleListItemClick(item)}>
-                      <h4>{item.consignee_name}</h4>
-                      <div>
-                        <p style={{display:"flex",gap:"3rem"}}>
-                          {item.pickup}   --   {item.dropoff}
-                        </p>
-                      </div>
-                    </li>
-                  ) 
-                })
-              )}
-        </ul>
-       </div>
+        </div>
+        <div className={styles.orderWindow}>
+          {!singleOrder ?(
+            console.log(orders),
+          <ul className={styles.orders}>
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              orders.map((item) => {
+                return (!transitOrDelivered && !item.delivered) ||
+                  (transitOrDelivered && item.delivered) ? (
+                  <li onClick={() => setSingleOrder((nullval) => item)}>
+                    <h4>{item.consignee_name}</h4>
+                    <div>
+                      <p style={{ display: 'flex', gap: '3rem' }}>
+                        {item.pickup} -- {item.dropoff}
+                      </p>
+                    </div>
+                  </li>
+                ) : (
+                 <></>
+                );
+              })
+            )}
+          </ul>
+          ):(<SingleOrder order={singleOrder} setSingleOrder={setSingleOrder}/> ) }
+        </div>
       </div>
       <div className={styles.btn_div}>
         <Link to="/user/neworder">
           <button onMouseOver={() => {}}>
             Ship New Order <Ship size={20} />
           </button>
-        </Link> */
-                </div>
+        </Link>{' '}
+        */
+      </div>
     </div>
   );
 };
@@ -130,7 +146,7 @@ const OrderPopup = ({ item }) => {
           `http://127.0.0.1:5000/api/orders/${item.booking_id}`,
           {
             headers: {
-              authorization: `${localStorage.getItem("user")}`,
+              authorization: `${localStorage.getItem('user')}`,
             },
           }
         );
@@ -141,17 +157,17 @@ const OrderPopup = ({ item }) => {
       } catch (err) {
         toast.error(err.message, {
           position: toast.POSITION.TOP_RIGHT,
-          className: "toast_message",
+          className: 'toast_message',
         });
         setIsLoading(false);
       }
     }
     getOrderDetails();
-  }, );
+  });
 
   return (
     <>
-      {" "}
+      {' '}
       {isLoading ? (
         <Spinner />
       ) : (
@@ -170,9 +186,8 @@ const OrderPopup = ({ item }) => {
   );
 };
 
-
-
- {/* <div className={styles.heading}>
+{
+  /* <div className={styles.heading}>
           <h1>
             Delivered
             <Check strokeWidth={2.9} />
@@ -249,4 +264,5 @@ const OrderPopup = ({ item }) => {
           <button onMouseOver={() => {}}>
             Ship New Order <Ship size={20} />
           </button>
-        </Link> */}
+        </Link> */
+}
---
+++ src/components/Modules/UserPanel/SingleOrder.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from 'react';
+import styles from './SingleOrder.module.css';
+import { toast } from 'react-toastify';
+import { MessageCircle, MoveLeft, Send, SendHorizonal } from 'lucide-react';
+
+export const SingleOrder = ({ order, setSingleOrder }) => {
+  const [orderSingle, setOrder] = useState({});
+  const[ImageData, setImageData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function getOrderDetails() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `http://127.0.0.1:5000/api/orders/${order.booking_id}`,
+          {
+            headers: {
+              authorization: `${localStorage.getItem('user')}`,
+            },
+          }
+        );
+        const response = await res.json();
+        console.log(response);
+        setOrder(response.booking);
+        const { image } = response.booking;
+        const uint8Array = new Uint8Array(image.data);
+
+        const blob = new Blob([uint8Array], { type: "image/jpg" }); // or 'image/jpeg'
+
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const base64Data = reader.result;
+          setImageData(base64Data);
+        };
+        reader.readAsDataURL(blob);
+        setIsLoading(false);
+        console.log(orderSingle);
+      } catch (err) {
+        toast.error(err.message, {
+          position: toast.POSITION.TOP_RIGHT,
+          className: 'toast_message',
+        });
+        setIsLoading(false);
+      }
+    }
+    getOrderDetails();
+  }, []);
+
+  return (
+    <div className={styles.order}>
+      <div className={styles.backArr}>
+        <button onClick={() => setSingleOrder((item) => null)}>
+          <MoveLeft />
+        </button>
+      </div>
+      <div className={styles.details}>
+        <div className={styles.imageDiv}>
+          <img src={ImageData} width={100} height={100} alt="order" />
+        </div>
+        <div className={styles.detailDiv}>
+          <h3
+            style={{ marginTop: '1rem', fontSize: '2.3rem', color: '#00c46a' }}
+          >
+            Order Details
+          </h3>
+          <h4 style={{ marginTop: '3rem' }}>
+            Consignee : {order.consignee_name}
+          </h4>
+          <h4 style={{ marginTop: '1.85rem' }}>
+            Ship Name : {orderSingle.shipName}
+          </h4>
+          <h4 style={{ marginTop: '1.85rem' }}>
+            Company Name : {orderSingle.name}
+          </h4>
+          <h4 style={{ marginTop: '1.85rem' }}>
+            Weight In Ton: {orderSingle.weight_in_tonne}
+          </h4>
+          <h4 style={{ marginTop: '1.85rem' }}>
+            Pickup Country : {order.pickup}
+          </h4>
+          <h4 style={{ marginTop: '1.85rem' }}>
+            Dropoff Country : {order.dropoff}
+          </h4>
+          {/* <h4>Ship Name : {orderSingle.name}</h4> */}
+        </div>
+      </div>
+
+      <div className={styles.company}>
+        <div className={styles.headingg}>
+          <h3
+            style={{ marginTop: '1rem', fontSize: '2.3rem', color: '#00c46a' }}
+          >
+            Company Details
+          </h3>
+        </div>
+        <div className={styles.companyDiv}>
+          <div>
+            <h4 style={{ marginTop: '3rem' }}>
+              Company Name : {orderSingle.name}
+            </h4>
+            <h4>Company Email : {orderSingle.email}</h4>
+            <h4>Company Phone : {orderSingle.phone}</h4>
+            <h4>Company Address : {orderSingle.address}</h4>
+          </div>
+
+          <div className={styles.btnDiv}>
+            <button className={styles.btn}>
+              Message <MessageCircle size={20} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* <h3>{order.consignee_name}</h3>
+        <div>
+            <p>
+                {order.pickup} -- {order.dropoff}
+            </p>
+        </div> */}
+    </div>
+  );
+};
